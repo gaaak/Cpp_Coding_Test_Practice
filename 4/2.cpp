893727af5d7bd0560925af29f829ac4c68d120fa
gaaak <75360063+gaaak@users.noreply.github.com>
2021-03-04 17:53:42 +0900

    Create 2.cpp

---
+++ 4/2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool check(int h, int m, int s)
+{
+	if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3)
+		return true;
+	return false;
+}
+
+
+int main()
+{
+	int hour, cnt = 0;
+	cin >> hour;
+	
+	for (int i = 0; i <= hour; i++)
+		for (int j = 0; j < 60; j++)
+			for (int k = 0; k < 60; k++)
+				if (check(i, j, k)) cnt++;
+	cout << cnt << "\n";
+
+	return 0;
+}
